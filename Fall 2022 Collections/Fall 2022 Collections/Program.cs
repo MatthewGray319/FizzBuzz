a856c2f1f4be1ad9aa8c3c07ae61324bfb25f945
Matthew Gray <matthewcharlesgray319@gmail.com>
2022-09-12 15:15:40 -0400

    Made changes

---
+++ Fall 2022 Collections/Fall 2022 Collections/Program.cs
@@ -0,0 +1,100 @@
+﻿// Collections
+
+// Arrays
+// fixed size, and of a particular type
+// indexed at 0
+
+var students = new string[13]; //creates an empty array with 13 slots to fill
+var instructors = new string[] { "Davis" , "Gavin", "El", "Rickie", "Cliff" };
+// because there are 5 things listed, the array's length is 5
+
+// variable = new type [] {comma separated values;
+// In C# an array's length and type cannot change.
+
+// Change the value at a slot in an array
+// name = value;
+// array[index] = value;
+students[7] = "Ashley";
+// student at index seven is updated to say "Ashley"
+// student at index seven is the 8th student
+// first thing in the list is arrray[0]
+//last thing is the list --> array[length - 1]
+Console.WriteLine(students.Length);
+
+// Lists
+// Dynamic size, still must be of one type
+// indexed at zero
+
+var groceries = new List<string>(); // sets up our new list, starting with no values, its count is 0
+groceries.Add("Pizza"); // our list contains 1 item, index = 0
+groceries.Add("Bananas"); // our list contains 2 items (banana has an index of 1)'
+groceries.Add("Grapes");
+groceries.Add("Potatoes");
+
+// Remove items from an array
+// list.Remove(item to remove);
+groceries.Remove("Bananas");
+Console.WriteLine(groceries.Count);
+Console.WriteLine(groceries[1]);
+
+// Remove at --> gives index to remove
+groceries.RemoveAt(1);
+
+groceries.Insert(1, "Bananas");  // shift items after this index up by 1, adds bananas to the list
+Console.WriteLine(groceries.Count);
+Console.WriteLine(groceries[1]);
+
+
+
+// How do we print out this list ???
+
+// Foreach Loops
+// Loops that run once for each item in a collection
+// rather than specifically having an index like previous loops, we have an object that we can look at and engage with
+// foreach(type item in collection)
+// {
+// code to execute
+// }
+foreach (var grocery in groceries)
+{
+    Console.WriteLine(grocery);
+}
+
+// Sometimes it's nice to have access to the index of a particular item as well as the item itself.
+// Use a for loop
+// Remember for an array the length is array.Length for a list it's list.count
+
+for (var i = 0; i < instructors.Length; i++)
+{
+    Console.WriteLine(instructors[i]);
+}
+
+// instructors[i] in a for loop is the same as the var instructor in a foreach loop
+
+// Dictionaries
+// Collection of key value pairs
+// keys - how we access elements
+// values - the values we get in return
+
+var weather = new Dictionary<string, float>();
+weather["Atlanta"] = 82.3f; // key is Atlanta, value is 82.3f
+weather["Cleveland"] = 71f;
+Console.WriteLine(weather["Atlanta"]);
+
+Console.WriteLine(weather.Count);
+
+
+// We have weather.add, weather.remove, and everything else
+
+foreach (var report in weather)
+{
+    Console.WriteLine(report);
+}
+
+foreach (var report in weather)
+{
+    Console.WriteLine("Temperature = " + report.Value);
+    Console.WriteLine("Location = " + report.Key);
+}
+
+// report is a key value pair object, meaning we have access to both the keys and values as we go through each item in our weather dictionary
---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿// Given a number N, print out the numbers 1 through N where the following replacements are made:
+// If a number is divible by 3, print "Fuzz".
+// If anumber is divisible by 5, print "Buzz".
+// If a number is divible by both print "FizzBuzz"
+// If none are the case, print the number
+
+var N = 47;
+for (var i = 1; i <= N; i++)
+{
+    if (i % 5 == 0 && i % 3 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    Console.WriteLine(i);
+
+}
